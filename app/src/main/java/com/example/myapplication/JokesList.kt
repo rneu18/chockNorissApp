a61d83ac87c216a5501accaa4505dbc9353436ca
rishikesh18 <rishi.neupane@gmail.com>
2019-05-13 13:20:58 -0400

    implemented infinite scrolling

--- app/src/main/java/com/example/myapplication/JokesList.kt
+++ app/src/main/java/com/example/myapplication/JokesList.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil.setContentView
@@ -10,12 +11,11 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_jokes_list.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import java.util.*
+import android.widget.AbsListView
+import kotlinx.coroutines.*
+
 
 class JokesList : AppCompatActivity() {
 
@@ -23,6 +23,12 @@ class JokesList : AppCompatActivity() {
     lateinit var myRecyclerView:RecyclerView
     lateinit var linearLayoutManager:LinearLayoutManager
     private lateinit var backToMenu: ImageView
+
+    var isScrolling:Boolean = false
+    var currentItems: Int = 0
+    var scrollOutItems: Int = 0
+    var totalItems: Int = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,11 +41,34 @@ class JokesList : AppCompatActivity() {
             startActivity(intent)
         }
 
+        initilizedRetrofit()
+
         myRecyclerView = findViewById(R.id.recyclerView)
-        recyclerView.setHasFixedSize(true)
+      //  recyclerView.setHasFixedSize(true)
         val linearLayoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = linearLayoutManager
-        initilizedRetrofit()
+        recyclerView.adapter = MyAdapter(myJokeList, this@JokesList)
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                    isScrolling = true
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                currentItems = linearLayoutManager.childCount
+                totalItems = linearLayoutManager.itemCount
+                scrollOutItems = linearLayoutManager.findFirstVisibleItemPosition()
+
+                if (isScrolling && currentItems + scrollOutItems === totalItems) {
+                    isScrolling = false
+                    initilizedRetrofit()
+                }
+            }
+        })
+
 
     }
 
@@ -62,8 +91,9 @@ class JokesList : AppCompatActivity() {
                             myJokeList.add(myRandomJoke)
                            // println("11111111111111111111111111111 $myJokeList")
                         }
-                        recyclerView.adapter = MyAdapter(myJokeList, this@JokesList)
+                      //  recyclerView.adapter = MyAdapter(myJokeList, this@JokesList)
                         (recyclerView.adapter as MyAdapter).notifyDataSetChanged()
+                       // MyAdapter.notifyDataSetChanged()
 
                     } else {
                         //  Toast.makeText(this@MainActivity, "Not connected!", Toast.LENGTH_SHORT).show()
@@ -79,5 +109,7 @@ class JokesList : AppCompatActivity() {
             }
         }
     }
+
+
 
 }
